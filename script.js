a04aff004b0e0f5d83dc8d921a36a7737e74c387
Mohammad Yusuf Khan <98419143+m-y-k@users.noreply.github.com>
2023-06-20 18:10:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+
+function updateTimer() {
+	var elem = document.getElementById("timer");
+	var currentTime = new Date();
+
+	var day = currentTime.getDate();
+	var month = currentTime.getMonth();
+	var year = currentTime.getFullYear();
+	var formattedDate = day + "/" + month + "/" + year;
+
+	var hour = currentTime.getHours();
+	var min = currentTime.getMinutes();
+	var sec = currentTime.getSeconds();
+	var formattedTime = hour + ":" + min + ":" + sec;
+
+	return formattedDate + ", " + formattedTime;
+}
+
+
+setInterval(updateTimer, 1000);
